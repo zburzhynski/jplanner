76ea2ff6b7b1ff7dccf18b344708610589d3e411
zburzhynski <zburzhynski@gmail.com>
2015-04-23 02:11:21 +0300

    Create / edit schedule events (draft).

---
+++ src/main/java/com/zburzhynski/jplanner/api/View.java
@@ -0,0 +1,35 @@
+package com.zburzhynski.jplanner.api;
+
+/**
+ * Contains view pages.
+ * <p/>
+ * Date: 22.04.15
+ *
+ * @author Vladimir Zburzhynski
+ */
+public enum View {
+
+    SCHEDULE_EVENTS("/pages/event/schedule-events?faces-redirect=true"),
+    SCHEDULE_EVENT("/pages/event/schedule-event?faces-redirect=true");
+
+    private String path;
+
+    /**
+     * Constructor.
+     *
+     * @param path path
+     */
+    private View(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Gets path.
+     *
+     * @return path
+     */
+    public String getPath() {
+        return path;
+    }
+
+}
--- src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/ScheduleBean.java
+++ src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/ScheduleBean.java
@@ -1,23 +1,87 @@
 package com.zburzhynski.jplanner.impl.jsf.bean;
 
+import com.zburzhynski.jplanner.api.View;
+import org.apache.commons.lang3.StringUtils;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
+import java.util.Date;
+import java.util.TimeZone;
+import javax.faces.application.NavigationHandler;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
- * Schedule bean.
+ * Schedule event bean.
  * <p/>
  * Date: 21.04.15
  *
  * @author Vladimir Zburzhynski
  */
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ScheduleBean {
 
     private ScheduleModel eventModel = new DefaultScheduleModel();
+
+    private ScheduleEvent event = new DefaultScheduleEvent();
+
+    /**
+     * Creates schedule event.
+     *
+     * @param selectEvent {@link SelectEvent}
+     */
+    public void createEvent(SelectEvent selectEvent) {
+        event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+        FacesContext fc = FacesContext.getCurrentInstance();
+        NavigationHandler nav = fc.getApplication().getNavigationHandler();
+        nav.handleNavigation(fc, null, View.SCHEDULE_EVENT.getPath());
+        fc.renderResponse();
+    }
+
+    /**
+     * Edits schedule event.
+     *
+     * @param selectEvent {@link SelectEvent}
+     */
+    public void editEvent(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+        FacesContext fc = FacesContext.getCurrentInstance();
+        NavigationHandler nav = fc.getApplication().getNavigationHandler();
+        nav.handleNavigation(fc, null, View.SCHEDULE_EVENT.getPath());
+        fc.renderResponse();
+    }
+
+    /**
+     * Saves schedule event.
+     *
+     * @return path for navigating
+     */
+    public String saveEvent() {
+        if (StringUtils.isBlank(event.getId())) {
+            eventModel.addEvent(event);
+        } else {
+            eventModel.updateEvent(event);
+        }
+        return View.SCHEDULE_EVENTS.getPath();
+    }
+
+    /**
+     * Cancel updates schedule event.
+     *
+     * @return path for navigating
+     */
+    public String cancelUpdateEvent() {
+        return View.SCHEDULE_EVENTS.getPath();
+    }
+
+    public String getTimeZone() {
+        return TimeZone.getDefault().getID();
+    }
 
     public ScheduleModel getEventModel() {
         return eventModel;
@@ -25,6 +89,14 @@ public class ScheduleBean {
 
     public void setEventModel(ScheduleModel eventModel) {
         this.eventModel = eventModel;
+    }
+
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
     }
 
 }
