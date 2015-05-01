9be2c6e8302969c4315314123335fff66ffc6804
zburzhynski <zburzhynski@gmail.com>
2015-05-01 23:40:03 +0300

    Main menu bean.

---
+++ src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/MainMenuBean.java
@@ -0,0 +1,29 @@
+package com.zburzhynski.jplanner.impl.jsf.bean;
+
+import com.zburzhynski.jplanner.api.domain.View;
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Main menu bean.
+ * <p/>
+ * Date: 01.05.15
+ *
+ * @author Vladimir Zburzhynski
+ */
+@ManagedBean
+@SessionScoped
+public class MainMenuBean implements Serializable {
+
+    /**
+     * Redirects to schedules.xhtml page.
+     *
+     * @return path to redirect
+     */
+    public String schedules() {
+        return View.SCHEDULE_EVENTS.getPath();
+    }
+
+}
