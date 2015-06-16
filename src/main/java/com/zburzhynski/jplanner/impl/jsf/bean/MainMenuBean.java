056273e2dcde1ed949f94a49c6d23e6cc30f7ec6
zburzhynski <zburzhynski@gmail.com>
2015-06-17 00:03:33 +0300

    Jdent integarion url.

--- src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/MainMenuBean.java
+++ src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/MainMenuBean.java
@@ -1,10 +1,15 @@
 package com.zburzhynski.jplanner.impl.jsf.bean;
 
 import com.zburzhynski.jplanner.api.domain.View;
+import com.zburzhynski.jplanner.impl.util.JsfUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 /**
  * Main menu bean.
@@ -17,6 +22,11 @@ import javax.faces.bean.ManagedBean;
 @SessionScoped
 public class MainMenuBean implements Serializable {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainMenuBean.class);
+
+    @ManagedProperty(value = "#{configBean}")
+    private ConfigBean configBean;
+
     /**
      * Redirects to schedules.xhtml page.
      *
@@ -24,6 +34,18 @@ public class MainMenuBean implements Serializable {
      */
     public String schedules() {
         return View.SCHEDULE_EVENTS.getPath();
+    }
+
+    /**
+     * External redirect to jdent.
+     */
+    public void jdent() {
+        String url = configBean.getJdentUrl();
+        try {
+            JsfUtils.externalRedirect(url);
+        } catch (IOException e) {
+            LOGGER.error("Can not redirect to url {}", url);
+        }
     }
 
     /**
@@ -42,6 +64,15 @@ public class MainMenuBean implements Serializable {
      */
     public String cabinets() {
         return View.CABINETS.getPath();
+    }
+
+    /**
+     * Sets config bean.
+     *
+     * @param configBean {@link ConfigBean} config bean to set
+     */
+    public void setConfigBean(ConfigBean configBean) {
+        this.configBean = configBean;
     }
 
 }
