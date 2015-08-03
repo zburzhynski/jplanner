f81070a0d9f40855d5d6c9f622b1199f61150310
zburzhynski <zburzhynski@gmail.com>
2015-08-03 23:04:21 +0300

    Version and changelog.

--- src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/VersionBean.java
+++ src/main/java/com/zburzhynski/jplanner/impl/jsf/bean/VersionBean.java
@@ -15,9 +15,9 @@ import javax.faces.bean.ManagedBean;
 @ApplicationScoped
 public class VersionBean implements Serializable {
 
-    private String version = "1.0.0";
+    private String version = "1.1.0";
 
-    private String versionDate = "29.07.2015";
+    private String versionDate = "03.08.2015";
 
     /**
      * Gets program version.
