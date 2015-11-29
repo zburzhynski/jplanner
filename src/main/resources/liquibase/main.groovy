db00409f8ee6d3aafb1c8e88af609ff1351a8baa
zburzhynski <zburzhynski@gmail.com>
2015-11-29 13:58:51 +0300

    Renamed 1.1.0 liquibase script.

--- src/main/resources/liquibase/main.groovy
+++ src/main/resources/liquibase/main.groovy
@@ -1,6 +1,6 @@
 databaseChangeLog {
     //jPlanner v1.0.0
     include(file: '1.0.0.groovy', relativeToChangelogFile: true)
-    //jPlanner v1.1.0
-    include(file: '1.1.0.groovy', relativeToChangelogFile: true)
+    //jPlanner v2.0.0
+    include(file: '2.0.0.groovy', relativeToChangelogFile: true)
 }
