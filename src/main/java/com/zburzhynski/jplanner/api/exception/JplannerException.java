f2889dbd508d5edd1e949c8bb10ba5fae90e6bfa
zburzhynski <zburzhynski@gmail.com>
2015-12-01 02:37:03 +0300

    Added custom exception.

---
+++ src/main/java/com/zburzhynski/jplanner/api/exception/JplannerException.java
@@ -0,0 +1,37 @@
+package com.zburzhynski.jplanner.api.exception;
+
+/**
+ * Jplanner base exception.
+ * <p/>
+ * Date: 30.11.2015
+ *
+ * @author Vladimir Zburzhynski
+ */
+public class JplannerException extends RuntimeException {
+
+    /**
+     * Default constructor.
+     */
+    public JplannerException() {
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message message
+     */
+    public JplannerException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message message
+     * @param cause cause
+     */
+    public JplannerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
