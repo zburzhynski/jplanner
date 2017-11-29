fee5964a98d51ead2ef431f6e80729827c0848eb
shevtsou <shevtsou@gmail.com>
2017-11-29 21:40:59 +0300

    https://bitbucket.org/zburzhynski/jplanner/issues/73/ Employee remove validation added.

---
+++ src/main/java/com/zburzhynski/jplanner/api/exception/LinkedAvailableResourceExistException.java
@@ -0,0 +1,11 @@
+package com.zburzhynski.jplanner.api.exception;
+
+/**
+ * Linked available resource exist exception.
+ * <p/>
+ * Date: 29.11.2017
+ *
+ * @author Nikita Shevtsov
+ */
+public class LinkedAvailableResourceExistException extends JplannerException {
+}
